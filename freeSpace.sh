50503d0d5379dfa39a02c4414f5f80672c1183d0
mrkocka <radics.pepe@gmail.com>
2023-05-24 13:31:49 +0200

    korrigált kód

--- freeSpace.sh
+++ freeSpace.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
-
-
 #The root directory max
 max=$(df -h / | awk 'NR==2{print $2}')
 
 #In use 
 hasznalt=$(df -h / | awk 'NR==2{print $3}')
 
-#Arithmetic operation on the two variables. 
-szabad=$(( ${max%[[:alpha:]]*} - ${hasznalt%[[:alpha:]]*} ))
-
-
+#Free 
+szabad=$(df -h / | awk 'NR==2{print $4}')
 
 #Values received.
 echo "Maximális: $max"
 echo "Használt: $hasznalt"
-echo "Szabad: $szabad"G""
+echo "Szabad: $szabad"
+
